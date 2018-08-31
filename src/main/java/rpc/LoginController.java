267347ff0782da491c7d9c8382c342058d3574ed
Mr Xv <625587553@qq.com>
2018-08-31 10:14:12 +0800

    Update LoginController.java

--- src/main/java/rpc/LoginController.java
+++ src/main/java/rpc/LoginController.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.ipc.RPC;
 public class LoginController {
 	public static void main(String[] args) throws Exception {
 		LoginService proxy = RPC.getProxy(LoginService.class, 1L,  new InetSocketAddress("it01",10000), new Configuration());
-		String login = proxy.login("你大爷 登录Liunx 成功了吗?", "123123");
+		String login = proxy.login("你 登录Liunx 成功了吗?", "123123");
 		System.out.println(login);
 	}
 }
